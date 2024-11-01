1b76a5eb078d2333776c8930a06e99c8e75d497e
SihagBots <sihagamit308@gmail.com>
2024-11-01 10:35:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002074410795')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/SihagBots_Discussion') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY' False)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
